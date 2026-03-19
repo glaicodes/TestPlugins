c11bafd537a29cabd3a507afbfbce9cf2c30d876
glai <75806392+glaicodes@users.noreply.github.com>
2026-03-19 23:31:09 +0300

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,9 @@
-rootProject.name = "CloudstreamPlugins"
+// settings.gradle.kts
+// Add every new provider folder here.
 
-// This file sets what projects are included.
-// All new projects should get automatically included unless specified in the "disabled" variable.
+rootProject.name = "AnizleExtension"
 
-val disabled = listOf<String>()
-
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
-    }
-}
-
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+// Auto-include every sub-folder that has a build.gradle.kts
+File(rootDir, ".").listFiles()
+    ?.filter { it.isDirectory && File(it, "build.gradle.kts").exists() }
+    ?.forEach { include(it.name) }
