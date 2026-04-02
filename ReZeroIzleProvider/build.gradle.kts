57699eaa3c6313d85a6b7b00eab06ba8a6611b2a
glai <75806392+glaicodes@users.noreply.github.com>
2026-04-02 21:23:10 +0300

    Add files via upload

---
+++ ReZeroIzleProvider/build.gradle.kts
@@ -0,0 +1,23 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    // All of the following are optional, remove any line you don't need
+    description = "Re:Zero kara Hajimeru Isekai Seikatsu — Türkçe altyazılı, Google Drive üzerinden."
+    authors = listOf("a")
+
+    /**
+     * Status of the plugin.
+     * 0: down
+     * 1: ok
+     * 2: slow
+     * 3: beta only
+     */
+    status = 1 // will be 3 if not main
+
+    tvTypes = listOf("Anime", "OVA")
+
+    iconUrl = "https://rezeroizle.com/images/icon.png"
+
+    language = "tr"
+}
