fbcecf49070b6d2eb10ba97db7df06e1a4a56abf
glai <75806392+glaicodes@users.noreply.github.com>
2026-07-12 03:57:39 +0300

    Add files via upload

--- ReZeroIzleProvider/src/main/kotlin/com/a/ReZeroIzle/ReZeroIzlePlugin.kt
+++ ReZeroIzleProvider/src/main/kotlin/com/a/ReZeroIzle/ReZeroIzlePlugin.kt
@@ -1,12 +1,11 @@
 package com.a.rezeroizle
 
-import android.content.Context
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class ReZeroIzlePlugin : Plugin() {
-    override fun load(context: Context) {
+class ReZeroIzlePlugin : BasePlugin() {
+    override fun load() {
         registerMainAPI(ReZeroIzleProvider())
     }
 }
