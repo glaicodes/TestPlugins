a48854b8c88b71b92d9fc83e3a49e5aad4767d60
glai <75806392+glaicodes@users.noreply.github.com>
2026-09-17 13:13:24 +0300

    Add files via upload

---
+++ AnizmProvider/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 1
+
+cloudstream {
+    language    = "tr"
+    description = "Anizm (anizm.net) - Türkçe altyazılı anime"
+    authors     = listOf(" ")
+
+    /**
+     * Status:
+     *  0 = Down
+     *  1 = Working
+     *  2 = Slow
+     *  3 = Beta only
+     */
+    status  = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
+
+    iconUrl = "https://anizm.net/favicon.ico"
+}
